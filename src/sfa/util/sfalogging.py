4c33694d5b6dc9658c929e5c8fda200f042eeee7
Tom Mitchell <tmitchell@bbn.com>
2010-04-26 13:07:44 -0400

    Incorporated PlanetLab revision 17781 from geni-api branch.
    Added PlanetLab copyright message prior to commit, but no code
    changes were made from the PlanetLab sources.

--- src/sfa/util/sfalogging.py
+++ src/sfa/util/sfalogging.py
@@ -28,11 +28,11 @@ SFA_HTTPD_ACCESS_LOGFILE='/tmp/sfa_httpd_access.log'
 logger=logging.getLogger()
 logger.setLevel(logging.INFO)
 try:
-    logfile=logging.FileHandler(SFA_ACCESS_LOGFILE)
+     logfile=logging.FileHandler(SFA_ACCESS_LOGFILE)
 except IOError:
-    # This is usually a permissions error becaue the file is
-    # owned by root, but httpd is trying to access it.
-    logfile=logging.FileHandler(SFA_HTTPD_ACCESS_LOGFILE)
+     # This is usually a permissions error becaue the file is
+     # owned by root, but httpd is trying to access it.
+     logfile=logging.FileHandler(SFA_HTTPD_ACCESS_LOGFILE)
 formatter = logging.Formatter("%(asctime)s - %(message)s")
 logfile.setFormatter(formatter)
 logger.addHandler(logfile)
