502c104e311c9a103e4e0cc8e3d262a9aa8143bc
Tom Mitchell <tmitchell@bbn.com>
2010-05-12 15:05:32 -0400

    Added an optional timeout argument to make_client.
    If timeout is passed, use the value as the timeout for the underlying
    socket. This will avoid waiting for xml rpc servers that aren't really
    there or are misbehaving.

--- src/geni/omni/xmlrpc/client.py
+++ src/geni/omni/xmlrpc/client.py
@@ -2,22 +2,28 @@ import xmlrpclib
 
 class SafeTransportWithCert(xmlrpclib.SafeTransport):
 
-    def __init__(self, use_datetime=0, keyfile=None, certfile=None):
+    def __init__(self, use_datetime=0, keyfile=None, certfile=None,
+                 timeout=None):
         xmlrpclib.SafeTransport.__init__(self, use_datetime)
         self.__x509 = dict()
         if keyfile:
             self.__x509['key_file'] = keyfile
         if certfile:
             self.__x509['cert_file'] = certfile
+        self._timeout = timeout
 
     def make_connection(self, host):
         host_tuple = (host, self.__x509)
-        return xmlrpclib.SafeTransport.make_connection(self, host_tuple)
+        conn = xmlrpclib.SafeTransport.make_connection(self, host_tuple)
+        if self._timeout:
+            conn._conn.timeout = self._timeout
+        return conn
 
-def make_client(url, keyfile, certfile, verbose=False):
+def make_client(url, keyfile, certfile, verbose=False, timeout=None):
     """Create an SSL connection to an XML RPC server.
     Returns the XML RPC server proxy.
     """
-    cert_transport = SafeTransportWithCert(keyfile=keyfile, certfile=certfile)
+    cert_transport = SafeTransportWithCert(keyfile=keyfile, certfile=certfile,
+                                           timeout=timeout)
     return xmlrpclib.ServerProxy(url, transport=cert_transport,
                                  verbose=verbose)
