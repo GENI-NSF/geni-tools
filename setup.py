59dbb0c9282d6f64fe6897807dc19475d64ec603
Josh <jkarlin@rabbit.bbn.com>
2010-08-26 15:06:52 -0400

    updated setup.py to only include a couple of files from the 'geni' package

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 setup(name='gcf',
       version="1.0b1",
       package_dir={'': 'src'},                   
-      packages=['geni','geni.util','sfa','sfa.trust','sfa.util'],
+      packages=['geni.util','sfa','sfa.trust','sfa.util'],
+      py_modules=['geni.__init__', 'geni.SecureXMLRPCServer'],
       author='gpo',
       author_email='help@geni.net',
       url='http://www.geni.net',
       )
-      
