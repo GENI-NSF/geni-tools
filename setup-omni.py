19d846efc74c5ce2e494e81185f39c05b158c8e2
Josh <jkarlin@rabbit.bbn.com>
2010-08-31 13:50:40 -0400

    omni package works.. need to fix up config file location for servers

--- setup-omni.py
+++ setup-omni.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
 setup(name='omni',
       version="1.0.1",
-      package_dir={'': 'src/geni'},      
-      packages=['omni','omni.frameworks','omni.omnispec', 'omni.util', 'omni.xmlrpc'],     
-      py_modules=['omni'],        
+      package_dir={'': 'src/'},      
+      packages=['geni.omni','geni.omni.frameworks','geni.omni.omnispec', 'geni.omni.util', 'geni.omni.xmlrpc'],     
+      scripts=['src/omni.py'],
       author='gpo',
       author_email='help@geni.net',
       url='http://www.geni.net',
-      requires=['gcf_lib',],
       provides=['omni',],
       )
