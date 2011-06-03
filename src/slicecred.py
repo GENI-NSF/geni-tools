df26137cb527bc5c5dfbd8fd1e186196f7910e5c
Tom Mitchell <tmitchell@bbn.com>
2011-06-03 13:01:03 -0400

    Improve slicecred.py.
    Print slice credential to stdout instead of to file. Remove all other
    stdout prints so output can be used by a calling program or redirected
    to file.

--- src/slicecred.py
+++ src/slicecred.py
@@ -43,7 +43,6 @@ def main(argv=None):
     keyfile = argv[3]
     certfile = argv[4]
     user_cert_file = argv[5]
-    outFileName = argv[6]
 
     confparser = ConfigParser.RawConfigParser()
     foundFile = confparser.read(configFile)
@@ -51,39 +50,24 @@ def main(argv=None):
         print "Unable to read file %s" % (configFile)
         return 2
     authority = confparser.get('global', 'base_name')
-    print "%r" % (authority)
 
     urn = URN(authority, "slice", sliceName).urn_string()
-    print "URN = %s" % (urn)
 
     (slice_cert, slice_keypair) = cert_util.create_cert(urn, keyfile, certfile)
-    print "slice_cert = %r; slice_keypair = %r" % (slice_cert, slice_keypair)
-    slice_cert.save_to_file("%s.pem" % (sliceName))
 
     user_cert = sfa.trust.gid.GID()
     user_cert.load_from_file(user_cert_file)
-    print "user_cert = %r" % (user_cert)
 
-    # 30 days
+    # 30 days - Expiration should be on the command line, and the
+    # lifetime should be computed.
     life_secs = 60 * 60 * 24 * 30
-
-    trust_roots = cred_util.CredentialVerifier(certfile).root_cert_files
-    print "trust_roots = %r" % (trust_roots)
-    print "trust_roots2 = %r" % ([certfile])
 
     slice_cred = cred_util.create_credential(user_cert, slice_cert, life_secs,
                                              'slice', keyfile, certfile,
-                                             #trust_roots)
                                              [certfile])
-    print "slice_cred = %r" % (slice_cred)
-    slice_cred.save_to_file(outFileName)
-    print "credential saved to = %r" % (outFileName)
+    print slice_cred.save_to_string()
 
     return 0
-
-def x_create_credential(caller_gid, object_gid, life_secs, typename,
-                      issuer_keyfile, issuer_certfile, trusted_roots):
-    pass
 
 if __name__ == "__main__":
     sys.exit(main())
