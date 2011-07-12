f77187ef9999e236e6e595540735d65f41e3cff3
Tom Mitchell <tmitchel@bbn.com>
2011-07-11 20:56:32 -0400

    Update version to 1.3.2

--- setup-omni.py
+++ setup-omni.py
@@ -23,7 +23,7 @@
 
 from distutils.core import setup
 setup(name='omni',
-      version="1.3.1",
+      version="1.3.2",
       package_dir={'': 'src/'},      
       packages=['omnilib','omnilib.frameworks','omnilib.omnispec', 'omnilib.util', 'omnilib.xmlrpc'],     
       data_files=[('/etc/omni/templates', ['omni_config'])],
