3182eecc8b9c6f329d196386d115be62d21b96af
Sarah Edwards <sedwards@bbn.com>
2011-07-19 11:47:10 -0400

    readyToLogin script now handles multiple keys per user and multiple users by adding -i key_file to the ssh command.

--- examples/readyToLogin.py
+++ examples/readyToLogin.py
@@ -36,8 +36,15 @@ from optparse import OptionParser
 #
 ################################################################################
 
+def findUsersAndKeys( config ):
+    """Look in omni_config for user and key information to pass to ssh"""
+    keyList = []
+    for user in config['users']:
+        userkeys = user['keys'].split(",")
+        keyList = keyList + userkeys
+    return keyList
 
-def sshIntoNodes( sliverStat, inXterm=True ):
+def sshIntoNodes( sliverStat, inXterm=True, keyList="" ):
     """Wrapper to determine type of node (PL, PG) and ssh into it."""
 
     if type(sliverStat) != type({}):
@@ -51,7 +58,7 @@ def sshIntoNodes( sliverStat, inXterm=True ):
             print ""
             print "="*80
             print "Aggregate [%s] has a PlanetLab sliver." % aggName
-            login = loginToPlanetlab( aggStat, inXterm )
+            login = loginToPlanetlab( aggStat, inXterm, keyList=keyList )
             if login is not None:
                 print login
             print "="*80
@@ -65,7 +72,7 @@ def sshIntoNodes( sliverStat, inXterm=True ):
             print ""
             print "="*80
             print "Aggregate [%s] has a ProtoGENI sliver.\n" % aggName
-            login = loginToProtoGENI( aggStat, inXterm )
+            login = loginToProtoGENI( aggStat, inXterm, keyList=keyList )
             if login is not None:
                 print login
             print "="*80
@@ -73,24 +80,28 @@ def sshIntoNodes( sliverStat, inXterm=True ):
         except:
             pass
 
-def loginToPlanetlab( sliverStat, inXterm=True ):
+def loginToPlanetlab( sliverStat, inXterm=True, keyList=[] ):
     """Print command to ssh into a PlanetLab host."""
     output = ""
     for resourceDict in sliverStat['geni_resources']: 
         if (not sliverStat['pl_login']) or (not resourceDict['pl_hostname']):
             return None
-        output += "%s's pl_boot_state is: \n\t%s\n" % (resourceDict['pl_hostname'],resourceDict['pl_boot_state'])
-        output += "Login using:\n\t"
+#        output += "%s's pl_boot_state is: \n\t%s\n" % (resourceDict['pl_hostname'],resourceDict['pl_boot_state'])
+        output += "Login using:\n"
 
-        if inXterm is True:
-            output += "xterm -e "
-        output += "ssh %s@%s" % ( sliverStat['pl_login'], resourceDict['pl_hostname'])
-        if inXterm is True:
-            output += " &"
-        output += "\n"
+        if len(keyList) == 0:
+            output = "There are no keys. You can not login to your nodes.\n"
+        for key in keyList:
+            output += "\t"
+            if inXterm is True:
+                output += "xterm -e "
+            output += "ssh -i %s %s@%s" % ( key, sliverStat['pl_login'], resourceDict['pl_hostname'])
+            if inXterm is True:
+                output += " &"
+            output += "\n"
     return output
 
-def loginToProtoGENI( sliverStat, inXterm=True ):
+def loginToProtoGENI( sliverStat, inXterm=True, keyList=[] ):
     """Print command to ssh into a ProtoGENI host."""
     output = ""
     for resourceDict in sliverStat['geni_resources']: 
@@ -100,14 +111,19 @@ def loginToProtoGENI( sliverStat, inXterm=True ):
                 if (not child.has_key('username')) or (not child.has_key('hostname')):
                     continue
                 output += "%s's geni_status is: \n\t%s\n" % (child['hostname'],resourceDict['geni_status'])
-                output += "Login using:\n\t"
+                output += "Login using:\n"
 
-                if inXterm is True:
-                    output += "xterm -e "
-                output += "ssh %s@%s" % ( child['username'], child['hostname'])
-                if inXterm is True:
-                    output += " &"
-        output += "\n"
+                if len(keyList) == 0:
+                    output = "There are no keys. You can not login to your nodes.\n"
+
+                for key in keyList:
+                    output += "\t"
+                    if inXterm is True:
+                        output += "xterm -e "
+                    output += "ssh -i %s %s@%s" % ( key, child['username'], child['hostname'])
+                    if inXterm is True:
+                        output += " &"
+                    output += "\n"
     return output
 def main(argv=None):
     parser = omni.getParser()
@@ -127,11 +143,15 @@ def main(argv=None):
         sys.exit("Must pass in slicename as argument of script.\nRun '%s -h' for more information."%sys.argv[0])
     
     # Run equivalent of 'omni.py sliverstatus username'
-    text, sliverStatus = omni.call( ['sliverstatus', slicename], options )
+    argv = ['sliverstatus', slicename]
+    text, sliverStatus = omni.call( argv, options )
+
+    framework, config, args, opts = omni.initialize( argv, options )
+    keyList = findUsersAndKeys( config )
 
     # Do Real Work
     # Determine how to SSH into nodes
-    sshIntoNodes( sliverStatus, inXterm=options.xterm )
+    sshIntoNodes( sliverStatus, inXterm=options.xterm, keyList=keyList )
         
 if __name__ == "__main__":
     sys.exit(main())
