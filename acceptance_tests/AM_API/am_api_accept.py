1db05bedb53766338f8184cdf84bc72176bb5601
Sarah Edwards <sedwards@bbn.com>
2012-02-22 14:39:08 -0500

    Remove word delete from assertion message.  Increase amount of RSpec printed on error in 2 spots.

--- acceptance_tests/AM_API/am_api_accept.py
+++ acceptance_tests/AM_API/am_api_accept.py
@@ -847,19 +847,19 @@ class Test(ut.OmniUnittest):
             # ListResources should return an RSpec containing no resources
             manifest = self.subtest_ListResources( slicename )
             self.assertTrue( rspec_util.is_wellformed_xml( manifest ),
-                  "Manifest RSpec returned by 'ListResources' on deleted slice '%s' " \
+                  "Manifest RSpec returned by 'ListResources' on slice '%s' " \
                              "expected to be wellformed XML file " \
                              "but was not. Return was: " \
                              "\n%s\n" \
                              "... edited for length ..."
-                         % (slicename, manifest[:100]))                         
+                         % (slicename, manifest[:1000]))                         
             self.assertFalse( rspec_util.has_child( manifest ),
-                  "Manifest RSpec returned by 'ListResources' on deleted slice '%s' " \
+                  "Manifest RSpec returned by 'ListResources' on slice '%s' " \
                               "expected to be empty " \
                               "but was not. Return was: " \
                               "\n%s\n" \
                               "... edited for length ..."
-                          % (slicename, manifest[:100]))
+                          % (slicename, manifest[:1000]))
         
         # Also repeated calls to DeleteSliver should now fail
         try:
