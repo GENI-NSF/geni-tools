2f6f5959387d084643e4c3e60845ecf773102211
Tom Mitchell <tmitchell@bbn.com>
2012-05-14 12:16:43 -0400

    Fix spacing in error messages.
    Add a space before line continuation so spaces appear between words.

--- src/omni-configure.py
+++ src/omni-configure.py
@@ -260,8 +260,8 @@ def initialize(opts):
     # If framework is pgeni, check that the cert file is in the right place
     if not cmp(opts.framework,'pg'):
         if not os.path.exists(opts.cert):
-            sys.exit("Geni certificate not in '"+opts.cert+"'. \nMake sure you\
-place the .pem file that you downloaded from the Web UI there,\nor\
+            sys.exit("Geni certificate not in '"+opts.cert+"'. \nMake sure you \
+place the .pem file that you downloaded from the Web UI there,\nor \
 use the '-p' option to specify a custom location of the certificate.\n")
 
     # If framework is planetlab, check that the key are in the right place
@@ -271,7 +271,7 @@ use the '-p' option to specify a custom location of the certificate.\n")
 PlanetLab cert.\nIf you have a copy place it at '"+opts.cert+"', \nor \
 use the '-p' option to specify a custom location of the certificate.\n")
         if not os.path.exists(opts.plkey) :
-            sys.exit("\nPlanetLab private key not in '"+opts.plkey+"'. \nMake sure\
+            sys.exit("\nPlanetLab private key not in '"+opts.plkey+"'. \nMake sure \
 you place the private key registered with PlanetLab there or use\n\
 the '-k' option to specify a custom location for the key.\n")
     
