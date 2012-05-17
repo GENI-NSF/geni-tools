46061cba6946c52e61f6264f338b120cee43b7a0
Marshall Brinn <mbrinn@bbn.com>
2012-05-17 14:53:22 -0400

    Initial version of proxy AM

---
+++ src/gcf-proxy-test.py
@@ -0,0 +1,311 @@
+#!/usr/bin/env python
+
+#----------------------------------------------------------------------
+# Copyright (c) 2010 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+#----------------------------------------------------------------------
+"""
+Simple test client for testing the GENI GCF Clearinghouse and 
+AggregateManager.
+
+Run with "-h" flag to see usage and command line options.
+"""
+
+import sys
+
+# Check python version. Requires 2.6 or greater, but less than 3.
+if sys.version_info < (2, 6):
+    raise Exception('Must use python 2.6 or greater.')
+elif sys.version_info >= (3,):
+    raise Exception('Not python 3 ready')
+
+import base64
+import datetime
+import logging
+import optparse
+import os
+import random
+import xml.dom.minidom as minidom
+import xmlrpclib
+import zlib
+from geni.config import read_config
+from omnilib.xmlrpc.client import make_client
+import sfa.trust.credential as cred
+
+def getAbsPath(path):
+    """Return None or a normalized absolute path version of the argument string.
+    Does not check that the path exists."""
+    if path is None:
+        return None
+    if path.strip() == "":
+        return None
+    path = os.path.normcase(os.path.expanduser(path))
+    if os.path.isabs(path):
+        return path
+    else:
+        return os.path.abspath(path)
+
+def exercise_ch(host, port, keyfile, certfile):
+    url = 'https://%s' % (host)
+    if port:
+        url = '%s:%s' % (url, port)
+    server = make_client(url, keyfile, certfile)
+    print server
+    try:
+        print server.GetVersion()
+    except xmlrpclib.Error, v:
+        print 'ERROR', v
+    try:
+        print server.CreateSlice()
+    except xmlrpclib.Error, v:
+        print 'ERROR', v
+
+def verify_rspec(rspec):
+    # It should be parseable XML
+    # The top level node should be named 'rspec'
+    # The children of the top level node should all be named 'resource'
+    dom = minidom.parseString(rspec)
+    top = dom.documentElement
+    if top.tagName.lower() != 'rspec':
+        return None
+    return dom
+
+def test_create_sliver(server, slice_urn, slice_credential, dom):
+    print("SERVER = " + str(server));
+    print("URN = " + str(slice_urn));
+    print("SC = " + str(slice_credential));
+    print("DOM = " + str(dom));
+    print 'Testing CreateSliver...',
+    resources = dom.getElementsByTagName('resource')
+    print("RESOURCES = " + str(resources));
+    dom_impl = minidom.getDOMImplementation()
+    request_rspec = dom_impl.createDocument(None, 'rspec', None)
+    print("REQUEST_RSPEC = " + str(request_rspec));
+    top = request_rspec.documentElement
+    if resources.length == 0:
+        print 'failed: no resources available'
+    elif resources.length == 1:
+        top.appendChild(resources.item(0).cloneNode(True))
+    else:
+        # pick two at random
+        indices = range(resources.length)
+        for _ in range(2):
+            index = random.choice(indices)
+            indices.remove(index)
+            top.appendChild(resources.item(index).cloneNode(True))
+    users = [{'key':''}]
+    manifest_rspec = server.CreateSliver(slice_urn, slice_credential,
+                                         request_rspec.toxml(), users)
+    # TODO: verify manifest_rspec
+    logging.getLogger('gcf-test').debug(manifest_rspec)
+    print 'passed'
+
+def test_delete_sliver(server, slice_urn, slice_credential):
+    print 'Testing DeleteSliver...',
+    try:
+        result = server.DeleteSliver(slice_urn, slice_credential)
+        if result is True:
+            print 'passed'
+        else:
+            print 'failed'
+    except xmlrpclib.Error, v:
+        print 'ERROR', v
+
+def test_sliver_status(server, slice_urn, credentials):
+    print 'Testing SliverStatus...',
+    result = server.SliverStatus(slice_urn, credentials)
+#    import pprint
+#    pprint.pprint(result)
+    sliver_keys = frozenset(('geni_urn', 'geni_status', 'geni_resources'))
+    resource_keys = frozenset(('geni_urn', 'geni_status', 'geni_error'))
+    errors = list()
+    missing = sliver_keys - set(result.keys())
+    if missing:
+        errors.append('missing keys %r' % (missing))
+    if 'geni_resources' in result:
+        for resource in result['geni_resources']:
+            missing = resource_keys - set(resource.keys())
+            if missing:
+                errors.append('missing resource keys %r' % (missing))
+    if errors:
+        print 'failed'
+        for x in errors:
+            print '\t', x
+    else:
+        print 'passed'
+        
+    # Note expiration_time is in UTC
+def test_renew_sliver(server, slice_urn, credentials, expiration_time):
+    print 'Testing RenewSliver...',
+    result = server.RenewSliver(slice_urn, credentials, expiration_time)
+    if result is True or result is False:
+        print 'passed. (Result: %r)' % (result)
+    else:
+        print 'failed'
+        print 'returned %r instead of boolean value' % (result)
+
+def test_shutdown(server, slice_urn, credentials):
+    print 'Testing Shutdown...',
+    result = server.Shutdown(slice_urn, credentials)
+    if result is True or result is False:
+        print 'passed'
+    else:
+        print 'failed'
+        print 'returned %r instead of boolean value' % (result)
+
+def test_get_version(server):
+    print 'Testing GetVersion...',
+    # This next throws exceptions on errors
+    vdict = server.GetVersion()
+    if vdict['geni_api'] == 2:
+        print 'passed'
+    else:
+        print 'failed'
+
+def test_list_resources(server, credentials, compressed=False, available=True,
+                        slice_urn=None):
+    print 'Testing ListResources...',
+    options = dict(geni_compressed=compressed, geni_available=available)
+    if slice_urn:
+        options['geni_slice_urn'] = slice_urn
+    rspec = server.ListResources(credentials, options)
+    if compressed:
+        rspec = zlib.decompress(base64.b64decode(rspec))
+    logging.getLogger('gcf-test').debug(rspec)
+    dom = verify_rspec(rspec)
+    if dom:
+        print 'passed'
+    else:
+        print 'failed'
+    return dom
+
+def exercise_am(ch_server, am_server):
+
+    # Create a slice at the clearinghouse
+    slice_cred_string = ch_server.CreateSlice()
+    slice_credential = cred.Credential(string=slice_cred_string)
+    slice_gid = slice_credential.get_gid_object()
+    slice_urn = slice_gid.get_urn()
+    print 'Slice Creation SUCCESS: URN = %s' % (slice_urn)
+    
+    # Set up the array of credentials as just the slice credential
+    credentials = [slice_cred_string]
+
+    test_get_version(am_server)
+    dom = test_list_resources(am_server, credentials)
+    test_create_sliver(am_server, slice_urn, credentials, dom)
+    test_sliver_status(am_server, slice_urn, credentials)
+    test_list_resources(am_server, credentials, slice_urn=slice_urn)
+    
+    expiration = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    test_renew_sliver(am_server, slice_urn, credentials, expiration)
+    
+    test_delete_sliver(am_server, slice_urn, credentials)
+    
+    # Test compression on list resources
+    dom = test_list_resources(am_server, credentials, compressed=True,
+                              available=False)
+
+    # Now create a slice and shut it down instead of deleting it.
+    slice_cred_string = ch_server.CreateSlice()
+    slice_credential = cred.Credential(string=slice_cred_string)
+    slice_gid = slice_credential.get_gid_object()
+    slice_urn = slice_gid.get_urn()
+    print 'Second Slice URN = %s' % (slice_urn)
+    credentials = [slice_cred_string]
+    dom = test_list_resources(am_server, credentials)
+    test_create_sliver(am_server, slice_urn, credentials, dom)
+    test_shutdown(am_server, slice_urn, credentials)
+
+def parse_args(argv):
+    parser = optparse.OptionParser()
+    parser.add_option("-k", "--keyfile",
+                      help="experimenter key file name", metavar="FILE")
+    parser.add_option("-g", "--certfile",
+                      help="experimenter certificate file name", metavar="FILE")
+    parser.add_option("-c", "--configfile",  help="config file path", metavar="FILE")
+    
+    parser.add_option("--ch", 
+                      help="clearinghouse URL")
+    parser.add_option("--am",
+                      help="aggregate manager URL")
+    parser.add_option("--debug", action="store_true", default=False,
+                       help="enable debugging output")
+    parser.add_option("--debug-rpc", action="store_true", default=False,
+                      help="enable XML RPC debugging")
+    return parser.parse_args()
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    opts = parse_args(argv)[0]
+    level = logging.INFO
+    if opts.debug:
+        level = logging.DEBUG
+        
+    # Read in config file options, command line gets priority
+    global config
+    optspath = None
+    if not opts.configfile is None:
+        optspath = os.path.expanduser(opts.configfile)
+
+    config = read_config(optspath)  
+        
+    for (key,val) in config['gcf-test'].items():
+        if hasattr(opts,key) and getattr(opts,key) is None:
+            setattr(opts,key,val)
+        if not hasattr(opts,key):
+            setattr(opts,key,val)      
+    
+    # Determine the AM and CH hostnames from the config file
+    if getattr(opts,'ch') is None:
+        host = config['clearinghouse']['host']
+        port = config['clearinghouse']['port']
+        if not host.startswith('http'):
+            host = 'https://%s' % host.strip('/')
+        url = "%s:%s/" % (host,port)
+        setattr(opts,'ch',url)
+        
+    if getattr(opts,'am') is None:
+        host = config['proxy aggregate_manager']['host']
+        port = config['proxy aggregate_manager']['port']
+        if not host.startswith('http'):
+            host = 'https://%s' % host.strip('/')
+        url = "%s:%s/" % (host,port)
+        setattr(opts,'am',url)
+
+    logging.basicConfig(level=level)
+    logger = logging.getLogger('gcf-test')
+    if not opts.keyfile or not opts.certfile:
+        sys.exit('Missing required arguments -k for Key file and -c for cert file')
+
+    keyf = getAbsPath(opts.keyfile)
+    certf = getAbsPath(opts.certfile)
+    logger.info('CH Server is %s. Using keyfile %s, certfile %s', opts.ch, keyf, certf)
+    logger.info('AM Server is %s. Using keyfile %s, certfile %s', opts.am, keyf, certf)
+    ch_server = make_client(opts.ch, keyf, certf, opts.debug_rpc)
+    am_server = make_client(opts.am, keyf, certf, opts.debug_rpc)
+    exercise_am(ch_server, am_server)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ src/geni/am/proxyam.py
@@ -0,0 +1,167 @@
+import base64
+import datetime
+import dateutil.parser
+import logging
+import os
+import traceback;
+import uuid
+import xml.dom.minidom as minidom
+import zlib
+
+import geni
+from geni.util.urn_util import publicid_to_urn
+from geni.am.am2 import AggregateManager
+from geni.am.am2 import AggregateManagerServer
+from geni.am.am2 import ReferenceAggregateManager
+from geni.SecureXMLRPCServer import SecureXMLRPCServer
+from geni.pgch import invokeCH;
+from omnilib.xmlrpc.client import make_client
+from resource import Resource
+from aggregate import Aggregate
+import tempfile
+from fakevm import FakeVM
+
+SR_URL = "https://marilac.gpolab.bbn.com/sr/sr_controller.php"
+
+
+class ProxyAggregateManager(ReferenceAggregateManager):
+
+    "A manager that responds to AM API and passes on requests to another AM"
+
+    # URL of MA controller in CH
+    ma_url = None
+
+    # URL of actual AM to which we're connecting
+    am_url = None
+
+    # *** TO DO ***
+    # keep a cache of member_id => key/cert
+    # And a cache of connections
+
+    def __init__(self, am_url, root_cert, urn_authority):
+        super(ProxyAggregateManager, self).__init__(root_cert, urn_authority);
+        self.am_url = am_url
+#        print("SELF.AM_URL = " + self.am_url)
+        dictargs = dict(service_type=3) # Member Authority
+        ma_services = invokeCH(SR_URL, 'get_services_of_type', self.logger, dictargs);
+        if(ma_services['code'] == 0):
+            ma_service_row = ma_services['value'][0]
+            self.ma_url = ma_service_row['service_url']
+            # print("MA_URL " + str(self.ma_url)) 
+        self.logger = logging.getLogger('gcf.pxam')
+
+    # Helper function to create a proxy client that talks 
+    # to real AM using inside keys
+    def make_proxy_client(self):
+        pc = self._server.peercert;
+#        print(str(pc))
+        san = pc.get('subjectAltName');
+        uri = None
+        uuid = None
+        for e in san:
+            key = e[0];
+            value = e[1];
+            if(key == 'URI' and "IDN+" in value):
+                uri = value;
+            if(key == 'URI' and 'uuid' in value):
+                uuid_parts = value.split(':');
+                uuid = uuid_parts[2];
+#        print "URI = " + str(uri) +  " UUID = " + str(uuid)
+        args = dict(member_id = uuid)
+        row = invokeCH(self.ma_url, 'lookup_keys_and_certs', self.logger, args)
+        
+        if(row['code'] == 0):
+            row_raw = row['value'];
+            private_key = row_raw['private_key']
+            certificate = row_raw['certificate']
+            (key_fid, key_fname) = tempfile.mkstemp()
+            os.write(key_fid, private_key);
+            os.close(key_fid);
+            (cert_fid, cert_fname) = tempfile.mkstemp();
+            os.write(cert_fid, certificate);
+            os.close(cert_fid);
+            
+        client = make_client(self.am_url, key_fname, cert_fname)
+        client.key_fname = key_fname;
+        client.cert_fname = cert_fname;
+        return client;
+
+    def close_proxy_client(self, client):
+        os.unlink(client.key_fname);
+        os.unlink(client.cert_fname);
+
+    def GetVersion(self, options):
+        client = self.make_proxy_client();
+        client_ret = client.GetVersion();
+        self.close_proxy_client(client);
+        return client_ret;
+
+    def ListResources(self, credentials, options):
+        client = self.make_proxy_client();
+        # Why do I need to add this?
+        options['geni_rspec_version'] = dict(type='geni', version='3');
+        print("OPTS = " + str(options));
+        print("CREDS = " + str(credentials));
+        client_ret = client.ListResources(credentials, options);
+        print("ListResources.CLIENT_RET = " + str(client_ret));
+        # Why do I need to do this?
+        client_ret = client_ret['value'];
+        self.close_proxy_client(client);
+        return client_ret;
+
+    def CreateSliver(self, slice_urn, credentials, rspec, users, options):
+        print("URN = " + str(slice_urn));
+        print("OPTS = " + str(options));
+        print("CREDS = " + str(credentials));
+        print("RSPEC = " + str(rspec));
+        print("USERS = " + str(users));
+        client = self.make_proxy_client();
+        client_ret = client.CreateSliver(slice_urn, credentials, rspec, users, options);
+#       print("CLIENT_RET = " + str(client_ret));
+        self.close_proxy_client(client);
+        return client_ret;
+            
+    def DeleteSliver(self, slice_urn, credentials, options):
+        return dict(code=dict(geni_code=-1, am_type="gcf2", am_code=-1), 
+                    value="NOT_IMPLEMENTED", output="");
+
+    def SliverStatus(self, slice_urn, credentials, options):
+        return dict(code=dict(geni_code=-1, am_type="gcf2", am_code=-1), 
+                    value="NOT_IMPLEMENTED", output="");
+
+    def RenewSliver(self, slice_urn, credentials, expiration_time, options):
+        return dict(code=dict(geni_code=-1, am_type="gcf2", am_code=-1), 
+                    value="NOT_IMPLEMENTED", output="");
+
+    def Shutdown(self, slice_urn, credentials, options):
+        return dict(code=dict(geni_code=-1, am_type="gcf2", am_code=-1), 
+                    value="NOT_IMPLEMENTED", output="");
+
+
+
+class ProxyAggregateManagerServer(AggregateManagerServer):
+    "A server that provides the AM API to tools, but passes requests"
+    "to a real configured AM, after logging and authorizing"
+
+    def __init__(self, addr, am_url, keyfile=None, certfile=None,
+                 trust_roots_dir=None,
+                 ca_certs=None, base_name=None):
+        # ca_certs arg here must be a file of concatenated certs
+        if ca_certs is None:
+            raise Exception('Missing CA Certs')
+        elif not os.path.isfile(os.path.expanduser(ca_certs)):
+            raise Exception('CA Certs must be an existing file of accepted root certs: %s' % ca_certs)
+
+        delegate = ProxyAggregateManager(am_url, trust_roots_dir, base_name)
+        self._server = SecureXMLRPCServer(addr, keyfile=keyfile,
+                                          certfile=certfile, ca_certs=ca_certs)
+        self._server.register_instance(AggregateManager(delegate))
+        # Set the server on the delegate so it can access the
+        # client certificate.
+        delegate._server = self._server
+
+        if not base_name is None:
+            global RESOURCE_NAMESPACE
+            RESOURCE_NAMESPACE = base_name
+
+   
