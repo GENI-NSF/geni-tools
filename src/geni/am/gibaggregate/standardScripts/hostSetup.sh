6a31f74e02d67e8a43a02102b0ad5ae2962e8e45
Vic Thomas <vthomas@bbn.com>
2012-10-08 16:51:11 -0500

    hostSetup.sh: Added a bunch more commands that need to executed when the host     is set up.

--- src/geni/am/gibaggregate/standardScripts/hostSetup.sh
+++ src/geni/am/gibaggregate/standardScripts/hostSetup.sh
@@ -25,35 +25,68 @@
 
 
 # This script should be run just ONE TIME to configure the host container.
-# This script is run as part of the process of setting up a VirtualBox a VM 
-#  with GENI-in-a-Box.  GENI-in-a-Box users should not have to run this script
+# This script is run as part of the process of setting up a VirtualBox VM 
+#  with GENI-in-a-Box.  GENI-in-a-Box users should not have to run this script.
 
-# This script should be run by root
 
+# Copy the .desktop file to the proper place so the gib-startup.sh script is run when the user logs in
+mkdir -p ~/.config/autostart
+cp ~/gcf/gib-config-files/gibStart.desktop ~/.config/autostart
+
+# Make the .gcf directory and copy the gcf_config and omni_config files into
+#    this directory
+mkdir ~/.gcf
+cp ~/gcf/gib-config-files/gcf_config ~/.gcf
+cp ~/gcf/gib-config-files/omni_config ~/.gcf
+
+# Add ~/gcf/src to the user's path
+echo "" >> ~/.bash_profile
+echo "# Lines added by GENI-in-a-Box setup script" >> ~/.bash_profile
+echo "PATH=$PATH:$HOME/bin:$HOME/gcf/src" >> ~/.bash_profile
+echo "export PATH" >> ~/.bash_profile
+
+# Create keys for user alice with empty passphrase.  Move the private key
+#   to the .ssh directory
+ssh-keygen -t rsa -N "" -f ~/.gcf/alice
+mkdir ~/.ssh
+mv ~/.gcf/alice ~/.ssh
+
+# Make sure we have the Python package needed by gcf  installed
+sudo yum install m2crypto python-dateutil pyOpenSSL xmlsec1 xmlsec1-devel xmlsec1-openssl xmlsec1-openssl-devel
+
+# Run gen-certs to create CH, AM and alice certs.
+~/gcf/src/gen-certs.py
+
+# Extract the password for the user accounts in the VMs created by createsliver
+#   and write it to ~/.gcf/passwords
+echo "The following accounts share the same password:" > ~/.gcf/passwords
+echo "    - root on the GENI-in-a-Box VM," >>  ~/.gcf/passwords
+echo "    - gib account (the GENI-in-a-Box account you are automatically logged into)," >> ~/.gcf/passwords 
+echo "    - root on the experimenter nodes," >>  ~/.gcf/passwords
+echo "    - user accounts on the experimenter nodes." >>  ~/.gcf/passwords
+echo "" >> ~/.gcf/passwords
+echo "The password is:" >> ~/.gcf/passwords
+echo -n "    " >> ~/.gcf/passwords
+grep -i "rootpwd" ~/gcf/src/geni/am/gibaggregate/config.py | awk '{print $3}' | sed "s/'//g" >> ~/.gcf/passwords
+echo "" >> ~/.gcf/passwords
+echo "Please do not change this password." >> ~/.gcf/passwords
+ 
+# Copy example rspec files to ~/geni-in-a-box
+cp -r ~/gcf/rspec-examples ~/geni-in-a-box
+
+# Install the brctl package
+sudo yum -y install /usr/sbin/brctl
 
 # Add /etc/host entries so the GENI-in-a-Box aggregate manager and 
 #    resources can be referenced by name.
 # Save current copy of /etc/hosts
-cp /etc/hosts /etc/hosts.save   
-
+sudo cp /etc/hosts /etc/hosts.save 
 # Add geni-in-a-box.net as an alias for localhost
-cat /etc/hosts.save | sed 's/127.0.0.1[[:space:]]*localhost.localdomain[[:space:]]*localhost/& geni-in-a-box.net/' > /etc/hosts
-
+sudo sh -c "cat /etc/hosts.save | sed 's/127.0.0.1[[:space:]]*localhost.localdomain[[:space:]]*localhost/& geni-in-a-box.net/' > /etc/hosts"
 # Add entries for the VMs.  Use the IP address on the control network
-echo "10.0.1.101 pc101.geni-in-a-box.net pc101" >> /etc/hosts
-echo "10.0.1.102 pc102.geni-in-a-box.net pc102" >> /etc/hosts
-echo "10.0.1.103 pc103.geni-in-a-box.net pc103" >> /etc/hosts
-echo "10.0.1.104 pc104.geni-in-a-box.net pc104" >> /etc/hosts
-echo "10.0.1.105 pc105.geni-in-a-box.net pc105" >> /etc/hosts
-echo "10.0.1.106 pc106.geni-in-a-box.net pc106" >> /etc/hosts
-
-# Copy the .desktop file to the proper place so the gib-startup.sh script is run when the user logs in
-mkdir -p /home/gib/.config/autostart
-chmod 777 /home/gib/.config/autostart
-cp /home/gib/gcf/gib-config-files/gibStart.desktop /home/gib/.config/autostart
-
-# Extract the password for the user accounts in the VMs created by createsliver
-#   and write it to ~/.gcf/passwords
-echo "Password for user accounts created on VMs allocated to slice: \n" > /home/gib/.gcf/passwords
-grep -i "rootpwd" config.py | awk '{print $3}' | sed "s/'//g" >> /home/gib/.gcf/passwords
-chmod 777 /home/gib/.gcf/passwords
+sudo sh -c "echo '10.0.1.101 pc101.geni-in-a-box.net pc101' >> /etc/hosts"
+sudo sh -c "echo '10.0.1.102 pc102.geni-in-a-box.net pc102' >> /etc/hosts"
+sudo sh -c "echo '10.0.1.103 pc103.geni-in-a-box.net pc103' >> /etc/hosts"
+sudo sh -c "echo '10.0.1.104 pc104.geni-in-a-box.net pc104' >> /etc/hosts"
+sudo sh -c "echo '10.0.1.105 pc105.geni-in-a-box.net pc105' >> /etc/hosts"
+sudo sh -c "echo '10.0.1.106 pc106.geni-in-a-box.net pc106' >> /etc/hosts"
