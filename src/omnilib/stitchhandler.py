b1f96fcd0d10f02a3fb91d362194290914838658
Aaron Helsinger <ahelsing@bbn.com>
2013-01-24 12:16:27 -0500

    split out a handler for stitching work

---
+++ src/omnilib/stitchhandler.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+
+#----------------------------------------------------------------------
+# Copyright (c) 2013 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+#----------------------------------------------------------------------
+import logging
+import omni
+
+from omnilib.util import OmniError
+from omnilib.util.files import readFile
+
+class StitchingError(OmniError):
+    '''Errors due to stitching problems'''
+    pass
+
+# The main stitching class. Holds all the state about our attempt at doing stitching.
+class StitchingHandler(object):
+    def __init__(self, opts, config, logger):
+        # FIXME: Do I need / want a framework here? Should main get it?
+#        self.framework = framework
+        self.logger = logger
+        config['logger'] = logger
+        self.omni_config = config['omni']
+        self.config = config
+        # FIXME: Duplicate the options like am_api_accept does?
+        self.opts = opts # command line options as parsed
+#        self.GetVersionCache = None # The cache of GetVersion info in memory
+
+    def doStitching(self, args):
+        #[whatever my algorithm is from below]
+
+        # Get request RSpec
+        request = None
+        command = None
+        slicename = None
+        if len(args) == 0:
+            self._raise_omni_error("Expected 3 args: <command = createsliver or allocate> <slice name> <rspec file path/url>")
+        elif len(args) == 1:
+            command = args[0]
+        elif len(args) == 2:
+            command = args[0]
+            request = args[1]
+            # FIXME: Create a slice
+            omniargs = ["createslice", "stitcher"]
+            try:
+                (slicename, message) = omni.call(omniargs, self.opts)
+            except:
+                pass
+        else:
+            command = args[0]
+            slicename = args[1]
+            request = args[2]
+            if len(args) > 3:
+                self.logger.warn("Arguments %s ignored", args[3:])
+        self.logger.info("Command=%s, slice=%s, rspec=%s", command, slicename, request)
+
+        # Read in the rspec as a string
+        # FIXME
+        requestString = ""
+
+        if request:
+            try:
+                # read the rspec into a string, and add it to the rspecs dict
+                requestString = readFile(request)
+            except Exception, exc:
+                msg = 'Unable to read rspec file %s: %s' % (request, str(exc))
+                raise OmniError(msg)
+
+            #    # Test if the rspec is really json containing an RSpec, and pull out the right thing
+            #    requestString = amhandler.self._maybeGetRSpecFromStruct(requestString)
+
+            # confirmGoodRequest
+            # -- FIXME: this should be a function in rspec_util: is a request (schema, type), is parsable xml, passes rspeclint?
+            
+            # parseRequest
+            # -- FIXME: As elementTree stuff?
+            #    requestStruct = parseRequest(requestString, logger)
+        requestStruct = None
+            
+        # If this is not a real stitching thing, just let Omni handle this.
+        if not self.mustCallSCS(requestStruct):
+            return omni.call(args, self.opts)
+        # return omni.call
+
+        sliceurn = self.confirmSliceOK(slicename)
+    
+        # constructSCSArgs: slice_urn, rspec, options
+        # options wil eventually include a geni_routing_profile struct that inside has the hop_exclusion_list
+        scsArgs = self.constructSCSArgs(sliceurn, requestString)
+
+        # callSCS (which method will make the call, or read result if fakeMode)
+        scsRespone = self.callSCS(scsArgs)
+
+        # Check scsResponse: should be a struct (Code, value, output) where code geni_code should be 0 
+
+        # If error, return
+        # save expanded RSpec
+        # parseRequest
+        # parseWorkflow
+          # parse list of AMs, URNs, URLs - creating structs for AMs and hops
+          # check each AM reachable, and we know the URL/API version to use
+          # parse hop dependency tree, giving each hop an explicit hop#
+          # Construct AM dependency tree
+            # If hop1 depends on hop2 and hop1 AM != hop2 AM then hop1 AM depends on hop2 AM
+              # Also hop1 AM depends on all AMs that hop2 AM depends on
+            # Do that for all paths, for all hops
+
+        # Mark on each hop which hop# it imports VLANs from, if any. For each hop
+          # set min_distance = MAX_INTEGER
+          # set import_vlans_from_hop#=None
+          # if import_vlans=0, done
+          # for each hop it depends on:
+            # if on same domain, skip
+            # If this diff-domain hop has # whose distance from orig hop is less than previous saved #, then set min_distance and import_vlans_from_hop#
+          # If this hop has 0 dependencies but there are other hops at this AM in same path and AM does not do translation, then
+            # set this hop import_vlans_from_hop# to the value from the other hop if it has a value
+
+        # Check for AM dependency loops
+        # Check SCS output consistency in a subroutine:
+          # In each path: An AM with 1 hop must either _have_ dependencies or _be_ a dependency
+          # All AMs must be listed in workflow data at least once per path they are in
+
+        # Construct list of AMs with no unsatisfied dependencies
+        # if notScript, print AM dependency tree
+        # Do Main loop (below)
+          # Are all AMs marked reserved/done? Exit main loop
+          # Any AMs marked should-delete? Do Delete 1 AM
+          # Any AMs have all dependencies satisfied? For each, do Reserve 1 AM
+        # Do cleanup if any, construct return, return to stitcher.main (see above)
+          # Construct a unified manifest
+          # include AMs, URLs, API versions
+          # use code/value/output struct
+          # If error and have an expanded rquest from SCS, include that in output.
+          #   Or if particular AM had errors, ID the AMe and errors
+        return ""
+
+    def mainStichingLoop(self):
+        # FIXME: Need to put this in an object where I can get to a bunch of data objects?
+
+        # Check if done? (see elsewhere)
+        # Check threads exited
+        # Check for AMs with delete pending
+          # Construct omni args
+          # Mark delete in process
+          # omni.newthread_call
+        # Check for threads exited
+        # Check for AMs with redo pending
+          # FIXME: Is this a thing? Or is this same as reserve?
+        # Construct omni args
+        # Mark redo in process
+        # omni.newthread_call
+        # Check for threads exited
+        # Check for AMs to reserve, no remaining dependencies
+          # Construct omni args
+          # Mark reserve in process
+          # Omni.newthread_call
+        # Any ops in process?
+          # Since when op finishes we go to top of loop, and loop spawns things, then if not we must be done
+            # Confirm no deletes pending, redoes, or AMs not reserved
+            # Go to end state section
+        pass
+
+    def mustCallSCS(self, request):
+        # >=1 link in main body with >= 2 diff component_manager names and no shared_vlan extension
+        if request:
+            return True
+
+    def confirmSliceOK(self, slicename):
+        # FIXME: I need to get the framework and use it to do getslicecred, etc.
+        fw = omni.load_framework(self.config, self.opts)
+        
+        # Get slice URN from name
+        sliceurn = fw.slice_name_to_urn(slicename)
+        # return error on error
+        # Get slice cred
+        # FIXME: Maybe use handler_utils._get_slice_cred
+        #    slice_cred = fw.get_slice_cred(sliceurn)
+        # return error on error
+        # Ensure slice not expired
+        # handler_utils._print_slice_expiration(sliceurn, slicecred)
+        # amhandler._has_slice_expired(slice_cred) (which uses credutils.get_cred_exp)
+        # return error on error
+        
+        # return the slice urn
+        return sliceurn
+
+    def constructSCSArgs(self, sliceurn, requestRSpec, options=None):
+        # FIXME FIXME
+        args = [sliceurn, requestRSpec, options]
+        return args
+    
+    def _raise_omni_error( self, msg, err=OmniError, triple=None ):
+        msg2 = msg
+        if triple is not None:
+            msg2 += " "
+            msg2 += str(triple)
+        self.logger.error( msg2 )
+        if triple is None:
+            raise err, msg
+        else: 
+            raise err, (msg, triple)
+
--- src/stitcher.py
+++ src/stitcher.py
@@ -28,160 +28,90 @@ import logging
 import sys
 
 import omni
+from omnilib.util import OmniError
+from omnilib.stitchhandler import StitchingError, StitchingHandler
 
-# Useful bits of code....
-#         self.logger = omni.configure_logging(self.options_copy)
-#        parser.remove_option("-t")
-#        parser.set_defaults(logoutput='acceptance.log')
-# See omni_unittest.py#unittest_parser
+# Call is the way another script might call this.
+# It initializes the logger, options, config (using omni functions),
+# and then dispatches to the stitch handler
+def call(argv, options=None):
 
-def mustCallSCS(logger, request):
- # >=1 link in main body with >= 2 diff component_manager names and no shared_vlan extension
-    return True
+    if options is not None and not options.__class__==optparse.Values:
+        raise OmniError("Invalid options argument to call: must be an optparse.Values object")
+    
+    if argv is None or not type(argv) == list:
+        raise OmniError("Invalid argv argument to call: must be a list")
+    
+    ##############################################################################
+    # Get a parser from omni that understands omni options
+    ##############################################################################
+    parser = omni.getParser()
+    # update usage for help message
+    omni_usage = parser.get_usage()
+    parser.set_usage("\n" + "GENI Omni Stitching Tool\n" + "Copyright (c) 2013 Raytheon BBN Technologies\n" + 
+                     omni_usage+
+                     "\nstitcher.py does stitching if the call is createsliver or allocate, else it just calls Omni.\n")
 
-def confirmSliceOK(logger, args, options, config, slicename):
-    # FIXME: I need to get the framework and use it to do getslicecred, etc.
+   ##############################################################################
+    # Add additional optparse.OptionParser style options
+    # Be sure not to re-use options already in use by omni for
+    # different meanings, otherwise you'll raise an OptionConflictError
+    ##############################################################################
+    parser.add_option("--fakeModeDir",
+                      help="If supplied, use canned server responses from this directory",
+                      default=None)
+    parser.add_option("--scsURL",
+                      help="URL to the SCS service",
+                      default="http://oingo.dragon.maxgigapop.net:8081/geni/xmlrpc")
+    #  parser.add_option("--script",
+    #                    help="If supplied, a script is calling this",
+    #                    action="store_true", default=False)
+    
+    # Put our logs in a different file by default
+    parser.set_defaults(logoutput='stitcher.log')
+    
+    # options is an optparse.Values object, and args is a list
+    options, args = parser.parse_args(argv)
 
-    # Get slice URN from name
-    # return error on error
-    # Get slice cred
-    # return error on error
-    # Ensure slice not expired
-    # return error on error
-    pass
+    # Set up the logger
+    omni.configure_logging(options)
+    logger = logging.getLogger("stitcher")
 
-def doStitching(logger, options, config, args):
-#[whatever my algorithm is from below]
-    # Get request RSpec
-    request = None
-    command = None
-    slicename = None
-    if len(args) == 0:
-        logger.error("Expected 3 args: <command = createsliver or allocate> <slice name> <rspec file path/url>")
-    elif len(args) == 1:
-        command = args[0]
-    elif len(args) == 2:
-        command = args[0]
-        request = args[1]
-        # FIXME: Create a slice
-        omniargs = ["createslice", "stitcher"]
-        (slicename, message) = omni.call(omniargs, options)
-    else:
-        command = args[0]
-        slicename = args[1]
-        request = args[2]
-        if len(args) > 3:
-            logger.warn("Arguments %s ignored", args[3:])
-    logger.info("Command=%s, slice=%s, rspec=%s", command, slicename, request)
+    # We use the omni config file
+    config = omni.load_config(options, logger)
 
-    # Read in the rspec as a string
-    # FIXME
-    requestString = ""
+    logger.info("Using AM API version %d", options.api_version)
 
-    # confirmGoodRequest
-    # -- FIXME: this should be a function in rspec_util: is a request (schema, type), is parsable xml, passes rspeclint?
+    if options.fakeModeDir:
+        logger.info("Got Fake Mode Dir %s", options.fakeModeDir)
+        if not os.path.isDir(options.fakeModeDir):
+            logger.error("But that is not a directory!")
+            raise StitchingError("Fake Mod path not a directory: %s" % options.fakeModeDir)
 
-    # parseRequest
-    # -- FIXME: As elementTree stuff?
-#    requestStruct = parseRequest(requestString, logger)
-    requestStruct = None
+    if options.debug:
+        logger.info(omni.getSystemInfo())
+            
+    handler = StitchingHandler(options, config, logger)    
+    return handler.doStitching(args)
 
-    # If this is not a real stitching thing, just let Omni handle this.
-    if not mustCallSCS(logger, requestStruct):
-        return omni.call(args, options)
-        # return omni.call
-
-    confirmSliceOK(logger, args, options, config, slicename)
-
-    # constructSCSArgs: slice_urn, rspec, options
-
-    # callSCS (which method will make the call, or read result if fakeMode)
-
-    # If error, return
-    # save expanded RSpec
-    # parseRequest
-    # parseWorkflow
-    # includes creating structs for AMs and hops
-    # Includes checking each AM reachable and we know the URL/API version to use
-    # Construct AM dependency struct and list of AMs with no unsatisfied dependencies
-    # if notScript, print AM dependency tree
-    # Do Main loop (below)
-    # Do cleanup if any, construct return, return to stitcher.main (see above)
-    return ""
-
+# Goal of main is to call the 'call' method and print the result
 def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
 
     print "This is stitcher.py"
     # FIXME: Print other header stuff?
-    result = call(argv)
+    try:
+        result = call(argv)
     # FIXME: If called from a script, then anything here?
 #    if options.script:
         # return json
 #        return result
 #    else:
-    prettyResult = json.dumps(result, ensure_ascii=True, indent=2)
-    print prettyResult
-    return
-
-def call(argv=None):
-# read config file
-# extend Omni parser & parse args
-# init logger (takes a log config file that might push to a file with custom name)
-# set debug level possible
-# call doStitching
-
-  ##############################################################################
-  # Get a parser from omni that understands omni options
-  ##############################################################################
-  parser = omni.getParser()
-  # update usage for help message
-  omni_usage = parser.get_usage()
-  parser.set_usage("\n" + "GENI Omni Stitching Tool\n" + "Copyright (c) 2013 Raytheon BBN Technologies\n" + 
-                   omni_usage+
-                   "\nstitcher.py does stitching if the call is createsliver or allocate, else it just calls Omni.\n")
-
-  ##############################################################################
-  # Add additional optparse.OptionParser style options
-  # Be sure not to re-use options already in use by omni for
-  # different meanings, otherwise you'll raise an OptionConflictError
-  ##############################################################################
-  parser.add_option("--fakeModeDir",
-                    help="If supplied, use canned server responses from this directory",
-                    default=None)
-  parser.add_option("--scsURL",
-                    help="URL to the SCS service",
-                    default="http://oingo.dragon.maxgigapop.net:8081/geni/xmlrpc")
-#  parser.add_option("--script",
-#                    help="If supplied, a script is calling this",
-#                    action="store_true", default=False)
-
-  # Put our logs in a different file by default
-  parser.set_defaults(logoutput='stitcher.log')
-
-  # options is an optparse.Values object, and args is a list
-  options, args = parser.parse_args(sys.argv[1:])
-
-  # Set up the logger
-  omni.configure_logging(options)
-  logger = logging.getLogger("stitcher")
-
-  # We use the omni config file
-  config = omni.load_config(options, logger)
-
-  logger.info("Using AM API version %d", options.api_version)
-
-  if options.fakeModeDir:
-      logger.info("Got Fake Mode Dir %s", options.fakeModeDir)
-      if not os.path.isDir(options.fakeModeDir):
-          logger.error("But that is not a directory!")
-          # FIXME: how handle errors?
-          raise
-
-  if options.debug:
-      logger.info(omni.getSystemInfo())
-
-  return doStitching(logger, options, config, args)
-
+        prettyResult = json.dumps(result, ensure_ascii=True, indent=2)
+        print prettyResult
+    except OmniError, oe:
+        sys.exit(oe)
 
 if __name__ == "__main__":
   sys.exit(main())
