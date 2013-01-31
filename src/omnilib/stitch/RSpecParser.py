6d171c4ba108bd29e918e0f7b32de151b92e422d
Tom Mitchell <tmitchell@bbn.com>
2013-01-31 17:36:06 -0500

    Further overhaul of rspec parsing.
    Most parsing is done in the data objects. The parser now directs the
    parsing at a high level. Individual objects are responsible for
    extracting their salient information.

--- src/omnilib/stitch/RSpecParser.py
+++ src/omnilib/stitch/RSpecParser.py
@@ -25,44 +25,17 @@
 
 import sys
 import logging
-from xml.dom.minidom import parseString, Node, getDOMImplementation
+from xml.dom.minidom import parseString, getDOMImplementation
 from objects import *
 
-TEXT_NODE = 3 #Node.TEXT_NODE
-
-# Node tags
+# XML tag constants
 RSPEC_TAG = 'rspec'
-NODE_TAG = 'node'
-INTERFACE_TAG = 'interface'
-INTERFACE_REF_TAG = 'interface_ref'
-COMPONENT_MANAGER_TAG = 'component_manager'
-HOP_TAG = 'hop'
 LINK_TAG = 'link'
 STITCHING_TAG = 'stitching'
 PATH_TAG = 'path'
-TRAFFIC_ENGINEERING_METRIC_TAG = 'trafficEngineeringMetric'
-CAPACITY_TAG = 'capacity'
-SWITCHING_CAPABILITY_DESCRIPTOR_TAG = 'switchingCapabilityDescriptor'
-SWITCHING_CAP_TYPE_TAG = 'switchingcapType'
-ENCODING_TYPE_TAG = 'encodingType'
-SWITCHING_CAPABILITY_SPECIFIC_INFO_TAG = 'switchingCapabilitySpecificInfo'
-SWITCHING_CAPABILITY_SPECIFIC_INFO_L2SC_TAG = \
-    'switchingCapabilitySpecificInfo_L2sc'
-INTERFACE_MTU_TAG = 'interfaceMTU'
-VLAN_RANGE_AVAILABILITY_TAG = 'vlanRangeAvailability'
-SUGGESTED_VLAN_RANGE_TAG = 'suggestedVLANRange'
-VLAN_TRANSLATION_TAG = 'vlanTranslation'
-NEXT_HOP_TAG = 'nextHop'
-CAPABILITIES_TAG = 'capabilities'
-CAPABILITY_TAG = 'capability'
 
-# Attribute tags
-CLIENT_ID_TAG = 'client_id'
-EXCLUSIVE_TAG = 'exclusive'
+# This should go away, its value is no longer used
 LAST_UPDATE_TIME_TAG = "lastUpdateTime"
-ID_TAG = 'id'
-NAME_TAG = 'name'
-COMPONENT_MANAGER_ID_TAG = 'component_manager_id'
 
 class RSpecParser:
 
@@ -71,26 +44,26 @@ class RSpecParser:
         if verbose:
             self.logger.setLevel(logging.DEBUG)
         self._verbose = verbose
+        # The dom tree after parsing
+        self.dom = None
 
     def parse(self, data):
         self.dom = parseString(data)
-        import code
-        code.interact(local=locals())
         rspecs = self.dom.getElementsByTagName(RSPEC_TAG)
         if len(rspecs) != 1:
             raise Exception("Expected 1 rspec tag, got %d" % (len(rpsecs)))
         return self.parseRSpec(rspecs[0])
 
     def parseRSpec(self, rspec_element):
-        if rspec_element.nodeName != RSPEC_TAG: 
-            print "Illegal head element: %r" % (rspec_element)
-            return None
+        if rspec_element.nodeName != RSPEC_TAG:
+            msg = "parseRSpec got unexpected tag %s" % (rspec_element.tagName)
+            raise Exception(msg)
         links = []
         stitching = None
         for child in rspec_element.childNodes:
             if child.nodeName == LINK_TAG:
                 self.logger.debug("Parsing Link")
-                link = self.parseLink(child)
+                link = Link.fromDOM(child)
                 links.append(link)
             elif child.nodeName == STITCHING_TAG:
                 self.logger.debug("Parsing Stitching")
@@ -99,187 +72,15 @@ class RSpecParser:
         rspec.links = links
         return rspec
 
-    def parseLink(self, link_element):
-        link = Link.fromDOM(link_element)
-        return link
-
     def parseStitching(self, stitching_element):
         last_update_time = stitching_element.getAttribute(LAST_UPDATE_TIME_TAG)
         paths = []
         for child in stitching_element.childNodes:
             if child.nodeName == PATH_TAG:
-                path = self.parsePath(child)
+                path = Path.fromDOM(child)
                 paths.append(path)
-            elif child.nodeType == TEXT_NODE:
-                pass
-            else:
-                print "UNKNOWN TAG FOR STITCHING: " + str(child)
-        if self._verbose:
-            attribs = {LAST_UPDATE_TIME_TAG:last_update_time}
-            print "      STITCHING: " + str(attribs)
         stitching = Stitching(last_update_time, paths)
         return stitching
-
-    def parsePath(self, path_element):
-        id = path_element.getAttribute(ID_TAG)
-        path = Path(id) #URN?
-        hops = []
-        for child in path_element.childNodes:
-            if child.nodeName == HOP_TAG:
-                hop = self.parseHop(child, path)
-                hops.append(hop)
-            elif child.nodeType == TEXT_NODE:
-                pass
-            else:
-                print "UNKNOWN TAG FOR PATH %r: " % id + str(child)
-        if self._verbose:
-            attribs = {ID_TAG:id}
-            print "      PATH: " + str(attribs)
-        path.hops = hops
-        return path
-
-    def parseHop(self, hop_element, path):
-        id = hop_element.getAttribute(ID_TAG)
-        hop_link = None
-        next_hop = None
-        for child in hop_element.childNodes:
-            if child.nodeName == LINK_TAG:
-                hop_link = self.parseHopLink(child)
-            elif child.nodeName == NEXT_HOP_TAG:
-                next_hop_text = child.firstChild.nodeValue
-                if next_hop_text != 'null':
-                    next_hop = int(next_hop_text)
-            elif child.nodeType == TEXT_NODE:
-                pass
-            else:
-                print "UNKNOWN CHILD FOR HOP %r: " % id + str(child)
-        if self._verbose:
-            attribs = {ID_TAG:id, NEXT_HOP_TAG: next_hop}
-            print "      HOP: " + str(attribs)
-        hop = Hop(id, hop_link, next_hop, path)
-        return hop
-
-    def parseHopLink(self, hop_link_element):
-        id = hop_link_element.getAttribute(ID_TAG)
-        traffic_engineering_metric = None
-        capacity = None
-        switching_capability_descriptor = None
-        capabilities = None
-        for child in hop_link_element.childNodes:
-            if child.nodeName == TRAFFIC_ENGINEERING_METRIC_TAG:
-                traffic_engineering_metric = int(child.firstChild.nodeValue)
-            elif child.nodeName == CAPACITY_TAG:
-                capacity = int(child.firstChild.nodeValue)
-            elif child.nodeName == SWITCHING_CAPABILITY_DESCRIPTOR_TAG:
-                switching_capability_descriptor = \
-                    self.parseSwitchingCapabilityDescriptor(child)
-            elif child.nodeName == CAPABILITIES_TAG:
-                capabilities = self.parseCapabilities(child)
-            elif child.nodeType == TEXT_NODE:
-                pass
-            else:
-                print " UNKNOWN CHILD FOR HOP_LINK: " + str(child)
-        if self._verbose:
-            attribs = {ID_TAG:id, \
-                           TRAFFIC_ENGINEERING_METRIC_TAG: \
-                           traffic_engineering_metric, \
-                           CAPACITY_TAG:capacity}
-            print "      HOP_LINK: " + str(attribs)
-        hop_link = HopLink(id, traffic_engineering_metric, capacity, \
-                               switching_capability_descriptor, capabilities)
-        return hop_link
-
-    def parseSwitchingCapabilityDescriptor(self, scd_element):
-        switching_cap_type = None
-        encoding_type = None
-        switching_capability_specific_info = None
-        for child in scd_element.childNodes:
-            if child.nodeName == SWITCHING_CAP_TYPE_TAG:
-                switching_cap_type = child.firstChild.nodeValue
-            elif child.nodeName == ENCODING_TYPE_TAG:
-                encoding_type = child.firstChild.nodeValue
-            elif child.nodeName == SWITCHING_CAPABILITY_SPECIFIC_INFO_TAG:
-                switching_capability_specific_info = \
-                    self.parseSwitchingCapabilitySpecificInfo(child)
-            elif child.nodeType == TEXT_NODE:
-                pass
-            else:
-                print "UNKNOWN CHILD FOR SCD: " + str(child)
-        scd = \
-            SwitchingCapabilityDescriptor(switching_cap_type, \
-                                              encoding_type, \
-                                              switching_capability_specific_info)
-        return scd
-
-    def parseSwitchingCapabilitySpecificInfo(self, scsi_element):
-        scsi_l2sc = None
-        for child in scsi_element.childNodes:
-            if child.nodeName == SWITCHING_CAPABILITY_SPECIFIC_INFO_L2SC_TAG:
-                scsi_l2sc = \
-                    self.parseSwitchingCapabilitySpecificInfo_l2sc(child)
-            elif child.nodeType == TEXT_NODE:
-                pass
-            else:
-                print "UNKNOWN CHILD FOR SCSI: " + str(child)
-        scsi = SwitchingCapabilitySpecificInfo(scsi_l2sc)
-        return scsi
-
-    def parseSwitchingCapabilitySpecificInfo_l2sc(self, scsi_l2sc_element):
-        interface_mtu = None
-        vlan_range_availability = None
-        suggested_vlan_range = None
-        vlan_translation = None
-
-        for child in scsi_l2sc_element.childNodes:
-            if child.nodeName == INTERFACE_MTU_TAG:
-                interface_mtu = int(child.firstChild.nodeValue)
-            elif child.nodeName == VLAN_RANGE_AVAILABILITY_TAG:
-                vlan_range_availability = child.firstChild.nodeValue
-            elif child.nodeName == SUGGESTED_VLAN_RANGE_TAG:
-                suggested_vlan_range = child.firstChild.nodeValue
-            elif child.nodeName == VLAN_TRANSLATION_TAG:
-                vlan_translation_text = child.firstChild.nodeValue
-                vlan_translation = vlan_translation_text.lower() == 'true'
-            elif child.nodeType == TEXT_NODE:
-                pass
-            else:
-                print "UNKNOWN CHILD TIME FOR SCSI_L2SC: " + str(child)
-
-        if self._verbose:
-            attribs = {INTERFACE_MTU_TAG: interface_mtu, \
-                       VLAN_RANGE_AVAILABILITY_TAG:vlan_range_availability, \
-                       SUGGESTED_VLAN_RANGE_TAG:suggested_vlan_range, \
-                           VLAN_TRANSLATION_TAG:vlan_translation}
-            print "      SCSI_L2SC:: " + str(attribs)
-        scsi_l2sc = \
-            SwitchingCapabilitySpecificInfo_l2sc(interface_mtu, \
-                                                     vlan_range_availability, \
-                                                     suggested_vlan_range, \
-                                                     vlan_translation)
-        return scsi_l2sc
-        
-
-    def parseCapability(self, element):
-        for child in element.childNodes:
-            if child.nodeType == TEXT_NODE:
-                return child.data
-            else:
-                print "UNKNOWN CHILD FOR CAPABILITY: " + str(child)
-
-    def parseCapabilities(self, element):
-        capabilities = []
-        for child in element.childNodes:
-            if child.nodeName == CAPABILITY_TAG:
-                capabilities.append(self.parseCapability(child))
-            elif child.nodeType == TEXT_NODE:
-                pass
-            else:
-                print "UNKNOWN CHILD FOR CAPABILITIES: " + str(child)
-        return capabilities
-
-# To be replaced by real classes
-
-
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
