08ddd59cfdbaef804806a2c5bdf5180f567191fc
Tom Mitchell <tmitchell@bbn.com>
2013-02-01 15:02:09 -0500

    Remove obsolete code & data structures.

--- src/omnilib/stitch/objects.py
+++ src/omnilib/stitch/objects.py
@@ -59,15 +59,6 @@ class Path(GENIObject):
         self._hops = []
         self._aggregates = []
 
-    def toXML(self, doc, parent):
-        path_node = doc.createElement('path')
-        parent.appendChild(path_node)
-        path_node.setAttribute('id', self.id)
-        for hop in self._hops:
-            hop.toXML(doc, path_node)
-        for agg in self._aggregates:
-            agg.toXML(doc, path_node)
-
     @property
     def hops(self):
         return self._hops
@@ -101,14 +92,6 @@ class Stitching(GENIObject):
         self.last_update_time = str(last_update_time)
         self.paths = paths
 
-    def toXML(self, doc, parent):
-        stitch_node = doc.createElement('stitching')
-        parent.appendChild(stitch_node)
-        stitch_node.setAttribute('lastUpdateTime', self.last_update_time)
-        if self.paths:
-            for path in self.paths:
-                path.toXML(doc, stitch_node)
-
     def find_path(self, link_id):
         if self.paths:
             for path in self.paths:
@@ -139,11 +122,6 @@ class Aggregate(GENIObjectWithIDURN):
     def __str__(self):
         return "<Aggregate %r>" % (self.urn)
 
-    def toXML(self, doc, parent):
-        agg_node = doc.createElement('component_manager')
-        parent.appendChild(agg_node)
-        agg_node.setAttribute('name', self.id)
-        
     @property
     def hops(self):
         return self._hops
@@ -225,15 +203,6 @@ class Hop(object):
     def __str__(self):
         return "<Hop %r on path %r>" % (self.urn, self._path.id)
 
-    def toXML(self, doc, parent):
-        hop_node = doc.createElement('hop')
-        parent.appendChild(hop_node)
-        hop_node.setAttribute('id', self._id)
-        if self._hop_link:
-            self._hop_link.toXML(doc, hop_node)
-        next_hop = self._next_hop or "null"
-        hop_node.appendChild(createElementAndText(doc, 'nextHop', next_hop))
-
     @property
     def urn(self):
         return self._hop_link and self._hop_link.urn
@@ -270,45 +239,6 @@ class Hop(object):
         self._dependencies.append(hop)
 
 
-class Hop_Orig(GENIObjectWithIDURN):
-    '''Hop'''
-    __simpleProps__ = [ ['index', int], ['aggregate', Aggregate], ['path', Path], ['inProcess', bool], ['completed', bool], ['userRequested', bool] ]
-
-    def __init__(self, id, urn=None, index=None, aggregate=None, path=None, inProcess=None, completed=None, userRequested=None):
-        super(Hop, self).__init__(id, urn=urn)
-        self.index = index
-        self.aggregate = aggregate
-        self.path = path
-        self.inProcess = inProcess
-        self.completed = completed
-        self.userRequested = userRequested
-        self._dependsOn = []
-        self._copiesVlansTo = []
-
-    def toXML(self, doc, parent):
-        hop_node = doc.createElement('hop')
-        parent.appendChild(hop_node)
-        hop_node.setAttribute('id', self.id)
-        if path:
-            path.toXML(doc, hop_node)
-            
-    @property
-    def dependsOn(self):
-        return self._dependsOn
-
-    @property
-    def copiesVlansTo(self):
-        return self._copiesVlansTo
-
-    @dependsOn.setter
-    def dependsOn(self, hopList):
-        self._setListProp('dependsOn', hopList, Hop)
-
-    @copiesVlansTo.setter
-    def copiesVlansTo(self, hopList):
-        self._setListProp('copiesVlansTo', hopList, Hop)
-
-
 class RSpec(GENIObject):
     '''RSpec'''
     __simpleProps__ = [ ['stitching', Stitching] ]
@@ -319,23 +249,6 @@ class RSpec(GENIObject):
         self._nodes = []
         self._links = []
         self.dom = None
-
-    def toXML(self, doc, parent):
-        parent.setAttribute('xmlns', 'http://www.geni.net/resources/rspec/3')
-        parent.setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
-        schema_locations = "http://hpn.east.isi.edu/rspec/ext/stitch/0.1/ " + \
-            "http://hpn.east.isi.edu/rspec/ext/stitch/0.1/stitch-schema.xsd " + \
-            "http://www.geni.net/resources/rspec/3 " + \
-            "http://www.geni.net/resources/rspec/3/request.xsd "
-        parent.setAttribute('xsi:schemaLocation', schema_locations)
-        parent.setAttribute('type', 'request')
-
-        for node in self._nodes:
-            node.toXML(doc, parent)
-        for link in self._links:
-            link.toXML(doc, parent)
-        if self.stitching:
-            self.stitching.toXML(doc, parent)
 
     @property
     def nodes(self):
@@ -369,50 +282,6 @@ class RSpec(GENIObject):
         return None
 
 
-class Node(GENIObject):
-    __ID__ = validateTextLike
-    __simpleProps__ = [ ['client_id', str], ['exclusive', TrueFalse]]
-
-    def __init__(self, client_id, aggregate=None, exclusive=None, interfaces=None):
-        super(Node, self).__init__()
-        self.id = str(client_id)
-        self.aggregate = aggregate
-        self.exclusive =  validateTrueFalse(exclusive)
-        self._interfaces = []
-        if interfaces: self._interfaces = interfaces
-
-    def toXML(self, doc, parent):
-        node_node = doc.createElement('node')
-        parent.appendChild(node_node)
-        node_node.setAttribute('client_id', self.id)
-        node_node.setAttribute('component_manager_id', self.aggregate)
-        node_node.setAttribute('exclusive', str(self.exclusive).lower())
-        for interface in self._interfaces:
-            interface.toXML(doc, node_node)
-
-    @property
-    def interfaces(self):
-        return self._interfaces
-
-    @interfaces.setter
-    def interfaces(self, interfaceList):
-        self._setListProp('interfaces', interfaceList, Interface)
-
-    @property
-    def aggregate(self):
-        return self._aggregate
-
-    @aggregate.setter
-    def aggregate(self, agg):
-        if agg is None:
-            return None
-        if type(agg) == Aggregate:
-            self._aggregate = agg
-        elif type(agg) == URN or type(agg) == str or  type(agg) == unicode:
-            self._aggregate = validateURN(agg)
-        else:
-            raise TypeError("aggregate must be a valid Aggregate or a valid URN")
-
 class Link(GENIObject):
     __ID__ = validateTextLike
     __simpleProps__ = [ ['client_id', str]]
@@ -437,7 +306,7 @@ class Link(GENIObject):
                 aggs.append(agg)
             elif child.nodeName == cls.INTERFACE_REF_TAG:
                 c_id = child.getAttribute(cls.CLIENT_ID_TAG)
-                ir = Interface(c_id)
+                ir = InterfaceRef(c_id)
                 refs.append(ir)
             # FIXME: If the link has the shared_vlan extension, note this
         link = Link(client_id)
@@ -453,22 +322,13 @@ class Link(GENIObject):
         self._interfaces = []
         self.hasSharedVlan = False
 
-    def toXML(self, doc, parent):
-        link_node = doc.createElement('link')
-        parent.appendChild(link_node)
-        link_node.setAttribute('client_id', self.id)
-        for agg in self._aggregates:
-            agg.toXML(doc, link_node)
-        for interface in self._interfaces:
-            interface.toXML(doc, link_node)
-
     @property
     def interfaces(self):
         return self._interfaces
 
     @interfaces.setter
     def interfaces(self, interfaceList):
-        self._setListProp('interfaces', interfaceList, Interface)
+        self._setListProp('interfaces', interfaceList, InterfaceRef)
 
     @property
     def aggregates(self):
@@ -478,39 +338,11 @@ class Link(GENIObject):
     def aggregates(self, aggregateList):
         self._setListProp('aggregates', aggregateList, Aggregate)
 
-class Interface(GENIObject):
-#    __simpleProps__ = [ ['client_id', str]]
-#    __simpleProps__ = [ ['client_id', TextLike]]
-    __ID__ = validateTextLike
-    def __init__(self, client_id):
-        super(Interface, self).__init__()
-        self.id = client_id
 
-    def toXML(self, doc, parent):
-        interface_node = doc.createElement('interface')
-        parent.appendChild(interface_node)
-        interface_node.setAttribute('client_id', self.id)
-
-class InterfaceRef(Interface):
-
-     __ID__ = validateURN
+class InterfaceRef(object):
      def __init__(self, client_id):
-         super(InterfaceRef, self).__init__(client_id)
+         self.client_id = client_id
 
-     def toXML(self, doc, parent):
-        interface_ref_node = doc.createElement('interface_ref')
-        parent.appendChild(interface_ref_node)
-        interface_ref_node.setAttribute('client_id', self.id)
-
-# DELETE
-class ComponentManager(Aggregate):
-    pass
-#     __ID__ = validateText
-# #    __simpleProps__ = [ ['name', str]]
-#     def __init__(self, name):
-#         super(ComponentManager, self).__init__(name)
-# #        self.name = name
-        
 
 class HopLink(GENIObject):
 
@@ -548,84 +380,6 @@ class HopLink(GENIObject):
         self.vlan_range = ""
         self.vlan_suggested = None
 
-    def toXML(self, doc, parent):
-        link_node = doc.createElement('link')
-        link_node.setAttribute('id', self._id)
-        parent.appendChild(link_node)
-        link_node.appendChild(createElementAndText(doc,
-                                                   'trafficEngineeringMetric',
-                                                   self._traffic_engineering_metric))
-        link_node.appendChild(createElementAndText(doc,
-                                                   'capacity',
-                                                   self._capacity))
-        if self._switching_capability_descriptor:
-            self._switching_capability_descriptor.toXML(doc, link_node)
-        if self._capabilities:
-            cs_node = doc.createElement('capabilities')
-            for c in self._capabilities:
-                cs_node.appendChild(createElementAndText(doc, 'capability', c))
-            link_node.appendChild(cs_node)
-
     @property
     def urn(self):
         return self._id
-
-
-class SwitchingCapabilityDescriptor(GENIObject):
-    def __init__(self, switching_cap_type, coding_type, \
-                     switching_capability_specific_info):
-        super(SwitchingCapabilityDescriptor, self).__init__()
-        self._switching_cap_type = switching_cap_type
-        self._coding_type = coding_type
-        self._switching_capability_specific_info = switching_capability_specific_info
-
-    def toXML(self, doc, parent):
-        scd_node = doc.createElement('switchingCapabilityDescriptor')
-        parent.appendChild(scd_node)
-        scd_node.appendChild(createElementAndText(doc,
-                                                  'switchingcapType',
-                                                  self._switching_cap_type))
-        scd_node.appendChild(createElementAndText(doc,
-                                                  'encodingType',
-                                                  self._coding_type))
-        if self._switching_capability_specific_info:
-           self._switching_capability_specific_info.toXML(doc, scd_node)
-
-class SwitchingCapabilitySpecificInfo(GENIObject):
-    def __init__(self, switching_capability_specific_info_l2sc):
-        super(SwitchingCapabilitySpecificInfo, self).__init__()
-        self._switching_capability_specific_info_l2sc = switching_capability_specific_info_l2sc
-
-    def toXML(self, doc, parent):
-        scsi_node = doc.createElement('switchingCapabilitySpecificInfo')
-        parent.appendChild(scsi_node)
-        if self._switching_capability_specific_info_l2sc:
-            self._switching_capability_specific_info_l2sc.toXML(doc, scsi_node)
-
-class SwitchingCapabilitySpecificInfo_l2sc(GENIObject):
-    def __init__(self, interface_mtu, vlan_range_availability, \
-                     suggested_vlan_range, vlan_translation):
-        super(SwitchingCapabilitySpecificInfo_l2sc, self).__init__()
-        self._interface_mtu = interface_mtu
-        self._vlan_range_availability = vlan_range_availability
-        self._suggested_vlan_range = suggested_vlan_range
-        self._vlan_translation = vlan_translation
-
-    def toXML(self, doc, parent):
-        scsi_l2sc_node = doc.createElement('switchingCapabilitySpecificInfo_L2sc')
-        parent.appendChild(scsi_l2sc_node)
-        scsi_l2sc_node.appendChild(createElementAndText(doc,
-                                                        'interfaceMTU',
-                                                        self._interface_mtu))
-
-        scsi_l2sc_node.appendChild(createElementAndText(doc,
-                                                        'vlanRangeAvailability',
-                                                        self._vlan_range_availability))
-        scsi_l2sc_node.appendChild(createElementAndText(doc,
-                                                        'suggestedVLANRange',
-                                                        self._suggested_vlan_range))
-        # Convert bool string to lowercase
-        vlan_translation = str(self._vlan_translation).lower()
-        scsi_l2sc_node.appendChild(createElementAndText(doc,
-                                                        'vlanTranslation',
-                                                        vlan_translation))
