dd67e84fbebb44da37485745c2461972ecc70567
Aaron Helsinger <ahelsing@bbn.com>
2013-02-26 08:48:16 -0500

    rework combineLinks to include the right interface_ref on links, and comments to cover all sliver_ids for links.

--- src/omnilib/stitch/ManifestRSpecCombiner.py
+++ src/omnilib/stitch/ManifestRSpecCombiner.py
@@ -94,7 +94,7 @@ class ManifestRSpecCombiner:
                             child_cmid = child.getAttribute('component_manager_id')
                             child_client_id = child.getAttribute('client_id')
                             if child_cmid == urn and child_client_id == template_client_id:
-                                self.logger.debug("Replacing " + str(template_node) + " with " + str(child) + " " + child_cmid)
+                                #self.logger.debug("Replacing " + str(template_node) + " with " + str(child) + " " + child_cmid)
                                 doc_root.replaceChild(child, template_node)
 
     # Replace each link in dom_template with matching link from (an) AM with same URN
@@ -108,30 +108,154 @@ class ManifestRSpecCombiner:
                 link = child
                 # Get first 'component_manager' child element
 #                print "LINK = " + str(link) + " " + cmid
-                client_id = link.getAttribute('client_id')
-                if link.hasAttribute('sliver_id'):
-                    # This one is already OK
-                    continue
+                client_id = str(link.getAttribute('client_id'))
+                needSwap = False
+                if not link.hasAttribute('sliver_id') and not link.hasAttribute('vlantag'):
+                    needSwap = True
+#                    self.logger.debug("Link %s in template must be swapped", client_id)
+                else:
+#                    self.logger.debug("Link %s in template has a sliver_id or a vlantag", client_id)
+                    pass
+
+                # get list of all cmids on this link
+                # Only look at AMs that match
                 component_manager_elements = link.getElementsByTagName('component_manager')
-#                for cm in component_manager_elements: print "     " + str(cm) + " " + str(cm.getAttribute('name'))
-                # Here we just grab the first AM - could look for one that has a node I suppose
-                component_manager_element = link.getElementsByTagName('component_manager')[0]
-                cmid = component_manager_element.getAttribute('name')
-                didSwap = False
-                for am in ams_list:
-                    am_manifest_dom = am.manifestDom
-                    if am.urn == cmid:
-                        am_doc_root = am_manifest_dom.documentElement
-                        for child in am_doc_root.childNodes:
-                            if child.nodeType == Node.ELEMENT_NODE and \
-                                    child.localName == RSpecParser.LINK_TAG:
-                                if child.getAttribute('client_id') == client_id and \
-                                        child.hasAttribute('sliver_id'):
-                                    doc_root.replaceChild(child, link)
-                                    didSwap = True
-                                    break
-                    if didSwap:
+                cms = []
+                for cme in component_manager_elements:
+                   cms.append(str(cme.getAttribute('name')))
+#                self.logger.debug("Ams in Link %s: %s", client_id, cms)
+
+                # Get interface_ref elements that need to be swapped
+                intfs = {}
+                for intf in link.getElementsByTagName('interface_ref'):
+                    if not intf.hasAttribute('sliver_id') and not intf.hasAttribute('component_id'):
+                        intfs[str(intf.getAttribute('client_id'))] = intf
+#                        self.logger.debug("intfc_ref %s has no sliver_id or component_id", intf.getAttribute('client_id'))
+                    else:
+                        sid = None
+                        cid = None
+                        if intf.hasAttribute('component_id'):
+                            cid = intf.getAttribute('component_id')
+                        if intf.hasAttribute('sliver_id'):
+                            sid = intf.getAttribute('sliver_id')
+#                        self.logger.debug("intfc_ref %s has sliver_id %s, component_id %s", intf.getAttribute('client_id'), sid, cid)
+
+#                self.logger.debug("Interfaces we need to swap: %s", intfs)
+
+                if len(intfs) == 0 and not needSwap:
+                    continue
+
+                for agg in ams_list:
+                    if len(intfs) == 0 and not needSwap:
                         break
+                    if agg.urn not in cms:
+                        # Not a relevant aggregate
+#                        self.logger.debug("Skipping AM %s not involved in link %s", agg.urn, client_id)
+                        continue
+#                    else:
+#                        self.logger.debug("Looking at AM %s for link %s", agg.urn, client_id)
+                    man = agg.manifestDom
+                    link_elements = man.getElementsByTagName(RSpecParser.LINK_TAG)
+                    for link2 in link_elements:
+                        if len(intfs) == 0 and not needSwap:
+                            break
+                        # Get the link with a sliverid and the right client_id
+                        if str(link2.getAttribute('client_id')) == client_id and \
+                                link2.hasAttribute('vlantag'):
+#                            self.logger.debug("Found AM %s link %s that has vlantag %s", agg.urn, client_id, link2.getAttribute('vlantag'))
+                            if needSwap:
+#                                self.logger.debug("Swapping link in template with this element")
+                                doc_root.replaceChild(link2, link)
+                                needSwap = False
+                                # Need to pull out the irefs with a sliver id or component_id from link
+                                # Before completing this swap
+                                for intf in link.childNodes:
+                                    if intf.nodeType == Node.ELEMENT_NODE and \
+                                            intf.localName == 'interface_ref' and \
+                                            (intf.hasAttribute('sliver_id') or intf.hasAttribute('component_id')):
+                                        for intf2 in link2.childNodes:
+                                            if inf2.nodeType == Node.ELEMENT_NODE and \
+                                                    intf2.localName == 'interface_ref' and \
+                                                    str(intf2.getAttribute('client_id')) == str(intf.getAttribute('client_id')) and \
+                                                    (not intf2.hasAttribute('sliver_id') and not intf2.hasAttribute('component_id')):
+#                                                self.logger.debug("from old template saving iref %s", intf2.getAttribute('client_id'))
+                                                link2.replaceChild(intf, intf2)
+                                                break
+
+                                # Need to recreate intfs dict
+                                # Get interface_ref elements that need to be swapped
+                                intfs = {}
+                                for intf in link2.getElementsByTagName('interface_ref'):
+                                    if not intf.hasAttribute('sliver_id') and not intf.hasAttribute('component_id'):
+                                        intfs[str(intf.getAttribute('client_id'))] = intf
+#                                        self.logger.debug("intfc_ref %s has no sliver_id or component_id", intf.getAttribute('client_id'))
+                                    else:
+                                        sid = None
+                                        cid = None
+                                        if intf.hasAttribute('component_id'):
+                                            cid = intf.getAttribute('component_id')
+                                        if intf.hasAttribute('sliver_id'):
+                                            sid = intf.getAttribute('sliver_id')
+#                                        self.logger.debug("intfc_ref %s has sliver_id %s, component_id %s", intf.getAttribute('client_id'), sid, cid)
+#                                self.logger.debug("Interfaces we need to swap: %s", intfs)
+
+                                # Add a comment on link2 with link's sliver_id and vlan_tag
+                                lsid = None
+                                if link.hasAttribute('sliver_id'):
+                                    lsid = link.getAttribute('sliver_id')
+                                lvt = link.getAttribute('vlantag')
+                                comment_text = "AM %s: sliver_id=%s vlantag=%s" % (agg.urn, lsid, lvt)
+                                comment_element = dom_template.createComment(comment_text)
+                                link2.insertBefore(comment_element, link2.firstChild)
+
+                                link = link2
+                                break # out of loop over link2's in this inner AM looking for the right link
+
+                            # Look at this version of the lin's interface_refs. If any have
+                            # a sliver_id or component_id, then this is the version with manifest info
+                            # put it on the linke
+                            for intf in link2.childNodes:
+                                if intf.nodeType == Node.ELEMENT_NODE and \
+                                        intf.localName == "interface_ref" and \
+                                        (intf.hasAttribute('sliver_id') or intf.hasAttribute('component_id')):
+                                    cid = str(intf.getAttribute('client_id'))
+                                    if intfs.has_key(cid):
+                                        sid = None
+                                        compid = None
+                                        if intf.hasAttribute('component_id'):
+                                            compid = intf.getAttribute('component_id')
+                                        if intf.hasAttribute('sliver_id'):
+                                            sid = intf.getAttribute('sliver_id')
+#                                        self.logger.debug("replacing iref cid %s, sid %s, comp_id %s: %s for old %s", cid, sid, compid, intf, intfs[cid])
+                                        link.replaceChild(intf, intfs[cid])
+#                                        self.logger.debug("Copied iref %s from AM %s", cid, agg.urn)
+                                        del intfs[cid]
+                                # End of loop over this Aggs link's children, looking for i_refs
+
+                            # Add a comment on link with link2's sliver_id and vlan_tag
+                            lsid = None
+                            if link2.hasAttribute('sliver_id'):
+                                lsid = link2.getAttribute('sliver_id')
+                            lvt = link2.getAttribute('vlantag')
+                            comment_text = "AM %s: sliver_id=%s vlantag=%s" % (agg.urn, lsid, lvt)
+                            comment_element = dom_template.createComment(comment_text)
+                            link.insertBefore(comment_element, link.firstChild)
+
+                            break # out of loop over Aggs' elements
+#                        else:
+#                            acid = str(link2.getAttribute('client_id'))
+#                            self.logger.debug("In manifest for AM %s found link %s", agg.urn, acid)
+#                            if acid == client_id:
+#                                self.logger.debug("Found AM %s link %s that has no vlantag - so we skip", agg.urn, client_id)
+#                            else:
+#                                self.logger.debug("Found AM %s link %s (not the one I'm looking for)", agg.urn, acid)
+
+
+                    # End of loop over this Aggs elements looking for the particular link
+                # end of loop over aggs looking for manifest link entries
+            # End of block handling link elements
+        # end of loop over template manifest elements
+    # end of combineLinks
 
     # Take a list of ams 
     # and replace the hop in the dom_template with the appropriate
@@ -183,7 +307,10 @@ class ManifestRSpecCombiner:
         api_version = am.api_version
         user_requested = am.userRequested
         hops_info = [{'urn':hop._hop_link.urn, 'vlan_tag':str(hop._hop_link.vlan_suggested_manifest), 'path_id':hop.path.id, 'id':hop._id}  for hop in am._hops]
-        return {'urn':urn, 'url': url, 'api_version':api_version, 'user_requested':user_requested, 'hops_info':hops_info}
+        ret = {'urn':urn, 'url': url, 'api_version':api_version, 'user_requested':user_requested, 'hops_info':hops_info}
+        if am.pgLogUrl:
+            ret["PG Log URL"] = am.pgLogUrl
+        return ret
 
     # Replace the hop element in the template DOM with the hop element 
     # from the aggregate DOM that has the given HOP ID
@@ -210,7 +337,7 @@ class ManifestRSpecCombiner:
                     break
 
         if am_hop and template_hop:
-            self.logger.debug("Replacing " + str(template_hop) + " with " + str(am_hop))
+            #self.logger.debug("Replacing " + str(template_hop) + " with " + str(am_hop))
             template_path.replaceChild(am_hop, template_hop)
         else:
             self.logger.error ("Can't replace hop in template: AM HOP %s TEMPLATE HOP %s" % (am_hop, template_hop))
