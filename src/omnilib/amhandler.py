e84efe37a5b843c0c4edcf4d1737c68b9c511ff5
Aaron Helsinger <ahelsing@bbn.com>
2014-02-03 15:39:07 -0500

    renewsliver uses sliverstatus to find slivers whose info to update, if necessary to record them. Add extra args to update_sliver_info

--- src/omnilib/amhandler.py
+++ src/omnilib/amhandler.py
@@ -1768,6 +1768,9 @@ class AMCallHandler(object):
                 # FIXME: Info only?
                 self.logger.warn('Error recording new slivers in SA database')
                 self.logger.debug(e)
+#                import traceback
+#                self.logger.debug(traceback.format_exc())
+#                raise e
 
             # FIXME: When Tony revises the rspec, fix this test
             if result and '<RSpec' in result and 'type="SFA"' in result:
@@ -2612,8 +2615,36 @@ class AMCallHandler(object):
                     agg_urn = self._getURNForClient(client)
                     if urn_util.is_valid_urn(agg_urn):
                         sliver_urns = self.framework.db_find_sliver_urns(urn, agg_urn)
+                        # We only get here if the framework implements db_find_sliver_urns
+                        if not sliver_urns:
+                            sliver_urns = []
+
+                        # Use sliverstatus to augment the list of slivers in this slice at this AM
+                        # This way we catch slivers that were never recorded.
+                        # Only do this if we have 0 slivers, to limit times we incur the expense of
+                        # an extra AM API call.
+                        if len(sliver_urns) == 0:
+                            st = None
+                            try:
+                                args2 = [urn, creds]
+                                ops = self._build_options('SliverStatus', name, None)
+                                args2.append(ops)
+                                ((st, m), c) = self._api_call(client,
+                                                              "Sliverstatus of %s at %s" % (urn, agg_urn),
+                                                              'SliverStatus', args2)
+                                (streal, m2) = self._retrieve_value(st, m, self.framework)
+                                self.logger.debug("Got st %s", streal)
+                            except Exception, e:
+                                self.logger.debug("Failed Sliverstatus to list slivers after renew of %s at %s: %s", urn, agg_urn, e)
+                            if streal and isinstance(streal, dict) and streal.has_key('geni_resources'):
+                                for s in streal['geni_resources']:
+                                    self.logger.debug("Got s %s", s)
+                                    if s.has_key('geni_urn') and urn_util.is_valid_urn_bytype(s['geni_urn'], 'sliver'):
+                                        if not s['geni_urn'] in sliver_urns:
+                                            sliver_urns.append(s['geni_urn'])
+
                         for sliver_urn in sliver_urns:
-                            self.framework.db_update_sliver_info(sliver_urn,
+                            self.framework.db_update_sliver_info(agg_urn, urn, sliver_urn,
                                                                  newExp)
                     else:
                         self.logger.info("Not updating recorded sliver expirations - no valid AM URN known")
@@ -2623,6 +2654,8 @@ class AMCallHandler(object):
                     # FIXME: Only info?
                     self.logger.warn('Error updating sliver record in SA database')
                     self.logger.debug(e)
+                    import traceback
+                    self.logger.debug(traceback.format_exc())
 
                 if numClients == 1:
                     retVal += prStr + "\n"
@@ -2833,6 +2866,7 @@ class AMCallHandler(object):
 
                 # record results in SA database
                 try:
+                    agg_urn = self._getURNForClient(client)
                     slivers = self._getSliverResultList(res)
                     for sliver in slivers:
                         if isinstance(sliver, dict) and \
@@ -2843,14 +2877,16 @@ class AMCallHandler(object):
                             # are not yet in the DB
                             if sliver.has_key('geni_allocation_status') and \
                                     sliver['geni_allocation_status'] == 'geni_allocated':
+                                self.logger.debug("Not recording updated sliver that is only allocated: %s", sliver)
                                 continue
 
                             # FIXME: Exclude slivers in sliverFails (had errors)?
                             if sliver['geni_sliver_urn'] in sliverFails.keys():
+                                self.logger.debug("Not recording sliver that had renew error: %s", sliver)
                                 continue
 
                             self.framework.db_update_sliver_info \
-                             (sliver['geni_sliver_urn'], sliver['geni_expires'])
+                             (agg_urn, urn, sliver['geni_sliver_urn'], sliver['geni_expires'])
                 except NotImplementedError, nie:
                     self.logger.debug('Framework %s doesnt support recording slivers in SA database', self.config['selected_framework']['type'])
                 except Exception, e:
@@ -3340,6 +3376,8 @@ class AMCallHandler(object):
                     # Get the Agg URN for this client
                     agg_urn = self._getURNForClient(client)
                     if urn_util.is_valid_urn(agg_urn):
+                        # I'd like to be able to tell the SA to delete all slivers registered for
+                        # this slice/AM, but the API says sliver_urn is required
                         sliver_urns = self.framework.db_find_sliver_urns(urn, agg_urn)
                         for sliver_urn in sliver_urns:
                             self.framework.db_delete_sliver_info(sliver_urn)
@@ -3961,7 +3999,7 @@ class AMCallHandler(object):
         '''ProtoGENI's ListImages function: List the disk images created by the given user. 
         Takes a user urn or name. If no user is supplied, uses the caller's urn. 
         Gives a list of all images created by that user, including the URN 
-        for deleting the image. Return is a list of structs containing the url and urn of the iamge.
+        for deleting the image. Return is a list of structs containing the url and urn of the image.
         Note that you should invoke this at the AM where the images were created.
         See http://www.protogeni.net/trac/protogeni/wiki/ImageHowTo'''
 
