8fdf2ad04bdee1c9cda0b54ed57f535fe329d60d
Regina Hain <rrhain@bbn.com>
2014-02-28 15:17:50 -0500

    Adding versioning for Mac

--- mac_install/addAliases.sh
+++ mac_install/addAliases.sh
@@ -31,12 +31,12 @@
 ## Function that writes aliases to the specified rc file
 writeAliases () {
     echo $'\n' >> $1
-    echo "# Aliaes for commands in the GENI omniTools package" >> $1
-    echo "alias omni='/Applications/omniTools/omni.app/Contents/MacOS/omni'" >> $1
-    echo "alias stitcher='/Applications/omniTools/stitcher.app/Contents/MacOS/stitcher'" >> $1
-    echo "alias omni-configure='/Applications/omniTools/omni-configure.app/Contents/MacOS/omni-configure'" >> $1
-    echo "alias readyToLogin='/Applications/omniTools/readyToLogin.app/Contents/MacOS/readyToLogin'" >> $1
-    echo "alias clear-passphrases='/Applications/omniTools/clear-passphrases.app/Contents/MacOS/clear-passphrases'" >> $1
+    echo "# Aliaes for commands in the GENI omniTools-2.5 package" >> $1
+    echo "alias omni='/Applications/omniTools-2.5/omni.app/Contents/MacOS/omni'" >> $1
+    echo "alias stitcher='/Applications/omniTools-2.5/stitcher.app/Contents/MacOS/stitcher'" >> $1
+    echo "alias omni-configure='/Applications/omniTools-2.5/omni-configure.app/Contents/MacOS/omni-configure'" >> $1
+    echo "alias readyToLogin='/Applications/omniTools-2.5/readyToLogin.app/Contents/MacOS/readyToLogin'" >> $1
+    echo "alias clear-passphrases='/Applications/omniTools-2.5/clear-passphrases.app/Contents/MacOS/clear-passphrases'" >> $1
 }
 
 wroteAliases=false
