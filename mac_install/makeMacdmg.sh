532de5b09cedf0f902b1569519b2b32b9be6ada4
Regina Hain <rrhain@bbn.com>
2014-03-04 15:04:44 -0500

    sh script to automate some of the mac dmg process

---
+++ mac_install/makeMacdmg.sh
@@ -0,0 +1,42 @@
+SRCDIR=~/gcf
+DESTDIR=~/omniTools/omniTools-2.5
+COVERDIR=~/omniTools
+
+cd ${SRCDIR}/src
+py2applet --make-setup clear-passphrases.py 
+python setup.py py2app --no-chdir
+mv dist/clear-passphrases.app $DESTDIR
+rm -r dist build setup.py 
+py2applet --make-setup omni.py
+python setup.py py2app --no-chdir
+mv dist/omni.app $DESTDIR
+rm -r dist build setup.py 
+py2applet --make-setup omni-configure.py
+python setup.py py2app --no-chdir
+mv dist/omni-configure.app $DESTDIR
+rm -r dist build setup.py 
+py2applet --make-setup stitcher.py
+python setup.py py2app --no-chdir
+mv dist/stitcher.app $DESTDIR
+rm -r dist build setup.py 
+cd ../examples/
+py2applet --make-setup readyToLogin.py
+python setup.py py2app --no-chdir
+mv dist/readyToLogin.app $DESTDIR
+rm -r dist build setup.py 
+
+#copy License and make Applications link
+ln -s /Applications $COVERDIR
+cp ${SRCDIR}/windows_install/LICENSE.txt $COVERDIR
+cp ${SRCDIR}/windows_install/LICENSE.txt $DESTDIR
+
+#copy other files of interest
+cp ${SRCDIR}/mac_install/background.png $DESTDIR
+cp ${SRCDIR}/mac_install/addAliases.sh $DESTDIR
+cp ${SRCDIR}/mac_install/INSTALL.txt $COVERDIR
+
+#copy READMES
+cp ${SRCDIR}/README-clearpassphrases.txt $DESTDIR
+cp ${SRCDIR}/README-omni.txt $DESTDIR
+cp ${SRCDIR}/README-omniconfigure.txt $DESTDIR
+cp ${SRCDIR}/README-stitching.txt $DESTDIR
