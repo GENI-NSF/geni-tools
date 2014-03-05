f8929e232cbced2f92eed41680580890e841c857
Aaron Helsinger <ahelsing@bbn.com>
2014-03-05 09:40:37 -0500

    fix 2 typos

--- mac_install/addAliases.sh
+++ mac_install/addAliases.sh
@@ -31,7 +31,7 @@
 ## Function that writes aliases to the specified rc file
 writeAliases () {
     echo $'\n' >> $1
-    echo "# Aliaes for commands in the GENI omniTools-2.5 package" >> $1
+    echo "# Aliases for commands in the GENI omniTools-2.5 package" >> $1
     echo "alias omni='/Applications/omniTools-2.5/omni.app/Contents/MacOS/omni'" >> $1
     echo "alias stitcher='/Applications/omniTools-2.5/stitcher.app/Contents/MacOS/stitcher'" >> $1
     echo "alias omni-configure='/Applications/omniTools-2.5/omni-configure.app/Contents/MacOS/omni-configure'" >> $1
